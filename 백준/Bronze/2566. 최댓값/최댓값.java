b3d890e9d5ee1e042253cf4998e32541b35262ec
Anthony <jinung.dev@gmail.com>
2024-03-11 23:29:40 +0900

    [Bronze III] Title: 최댓값, Time: 156 ms, Memory: 16260 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[][] arr = new int[9][9];
+
+        for (int i = 0; i < 9; i++) {
+            String[] input = br.readLine().split(" ");
+            for (int j = 0; j < 9; j++) {
+                arr[i][j] = Integer.parseInt(input[j]);
+            }
+        }
+
+        int max = 0;
+        int x = 1;
+        int y = 1;
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (arr[i][j] > max) {
+                    max = arr[i][j];
+                    x = i + 1;
+                    y = j + 1;
+                }
+            }
+        }
+
+        bw.write(max + "\n" + x + " " + y);
+
+        bw.flush();
+        bw.close();
+    }
+}
